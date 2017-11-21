ba23c985fe9613b8ef849d8f5455463536f373d8
Roman-Labtech <labtechno.roman@yandex.ru>
2017-11-21 12:15:23 +0300

    Add files via upload

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -452,58 +452,16 @@ $$('.alert-text-title').on('click', function () {
 
 
 
-//Рекламный баннер
 
-var admobid = {}
-if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
-  admobid = {
-    banner: 'ca-app-pub-3940256099942544/6300978111',
-    interstitial: 'ca-app-pub-3940256099942544/1033173712',
-  }
-} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
-  admobid = {
-    banner: 'ca-app-pub-3940256099942544/2934735716',
-    interstitial: 'ca-app-pub-3940256099942544/4411468910',
-  }
+function fnradian(){
+document.FormThoo.ygol2.active==true;
+	
 }
 
-document.addEventListener('deviceready', function() {
-  admob.banner.config({
-    id: admobid.banner,
-    isTesting: true,
-    autoShow: true,
-  })
-  admob.banner.prepare()
 
-  admob.interstitial.config({
-    id: admobid.interstitial,
-    isTesting: true,
-    autoShow: false,
-  })
-  admob.interstitial.prepare()
-
-  document.getElementById('showAd').disabled = true
-  document.getElementById('showAd').onclick = function() {
-    admob.interstitial.show()
-  }
-
-}, false)
-
-document.addEventListener('admob.banner.events.LOAD_FAIL', function(event) {
-  console.log(event)
-})
-
-document.addEventListener('admob.interstitial.events.LOAD_FAIL', function(event) {
-  console.log(event)
-})
-
-document.addEventListener('admob.interstitial.events.LOAD', function(event) {
-  console.log(event)
-  document.getElementById('showAd').disabled = false
-})
-
-document.addEventListener('admob.interstitial.events.CLOSE', function(event) {
-  console.log(event)
-
-  admob.interstitial.prepare()
-}) 
+$$('.confirm-ok').on('click', function () {
+    myApp.confirm('Вы уверены?', function () {
+        myApp.alert('Вы нажали кнопку ОК');
+		navigator.app.exitApp();
+    });
+});
